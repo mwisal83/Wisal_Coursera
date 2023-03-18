aec37250bb8dc20aaa5daa764426ec642b5ab2d5
mwisal83 <92426996+mwisal83@users.noreply.github.com>
2023-03-18 15:35:10 +0500

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,3 +1,33 @@
+
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+
+/**
+ * @file stats.c
+ * @brief definitions and the implementations of some statistical functions
+ *
+ * Contains: main() - The main entry point for your program
+             print_statistics() - A function that prints the statistics of an array including minimum, maximum, mean, and median.
+             print_array() -  Given an array of data and a length, prints the array to the screen
+             find_median() - Given an array of data and a length, returns the median value
+             find_mean() -  Given an array of data and a length, returns the mean
+             find_maximum() -  Given an array of data and a length, returns the maximum
+             find_minimum() -  Given an array of data and a length, returns the minimum
+             sort_array() - Given an array of data and a length, sorts the array from largest to smallest. (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+ *
+ * @author Muhammad Wisal Khan
+ * @date 18.03.2023
+ *
+ */
+
 #include <stdio.h> 
 #include "stats.h"
   
